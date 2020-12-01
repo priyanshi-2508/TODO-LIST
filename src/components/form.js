2e730158e295500df97423e62c007c9a97e565e9
unknown <1510priyanshi@gmail.com>
2020-12-01 21:05:25 +0530

    Changed

--- src/components/form.js
+++ src/components/form.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Form = ({ setInputText }) => {
     //Here we can write JS code and functions
     const inputTextHandler = (e) => {
-        console.log(e.target.value);
+        //console.log(e.target.value);
         setInputText(e.target.value);
     };
     return(
